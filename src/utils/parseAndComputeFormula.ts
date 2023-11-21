7f0b4766fce651d61dad4f9911f02b46ac571140
fvergaracl <fvergaracl@gmail.com>
2023-11-21 11:29:10 +0100

    Fix formula calculation in parseAndComputeFormula function

--- src/utils/parseAndComputeFormula.ts
+++ src/utils/parseAndComputeFormula.ts
@@ -7,9 +7,9 @@ type StrategyParameters = {
 };
 
 type TaskData = {
-  TIME_INVESTED_LAST_TASK: number;
-  GLOBAL_AVERAGE: number;
-  USER_AVERAGE: number;
+  TIME_INVESTED_LAST_TASK?: number | undefined;
+  GLOBAL_AVERAGE?: number | undefined;
+  USER_AVERAGE?: number | undefined;
 };
 
 function parseAndComputeFormula(
@@ -17,35 +17,66 @@ function parseAndComputeFormula(
   params: StrategyParameters,
   taskData: TaskData
 ): number {
-  if (formula.includes("BASIC_POINTS + WEIGHT_GLOBAL_IMPROVE")) {
-    // Handling the first formula
-    const globalImprovement = Math.max(
-      0,
-      (taskData.GLOBAL_AVERAGE - taskData.TIME_INVESTED_LAST_TASK) /
-        taskData.GLOBAL_AVERAGE
-    );
-    const individualImprovement = Math.max(
-      0,
-      (taskData.USER_AVERAGE - taskData.TIME_INVESTED_LAST_TASK) /
-        taskData.USER_AVERAGE
-    );
-    return (
-      params.BASIC_POINTS +
-      params.WEIGHT_GLOBAL_IMPROVE * globalImprovement +
-      params.WEIGHT_INDIVIDUAL_IMPROVE * individualImprovement
-    );
-  } else if (
-    formula.includes("BASIC_POINTS + (TIME_INVESTED_LAST_TASK > USER_AVERAGE")
-  ) {
-    // Handling the second formula
-    return (
-      params.BASIC_POINTS +
-      (taskData.TIME_INVESTED_LAST_TASK > taskData.USER_AVERAGE
-        ? -params.SMALLER_BONUS
-        : params.BONUS_FACTOR)
-    );
-  } else {
-    // Default case if the formula does not match the expected patterns
-    return 0;
+  // FORMULA_BASIC_POINTS
+  // FORMULA_GLOBAL_AVERAGE_COMPARISON
+  // FORMULA_USER_AVERAGE_COMPARISON
+  // FORMULA_GLOBAL_AND_INDIVIDUAL_IMPROVEMENT
+  if (formula === "FORMULA_BASIC_POINTS") {
+    // BASIC_POINTS
+    return params.BASIC_POINTS;
   }
+  if (formula === "FORMULA_GLOBAL_AVERAGE_COMPARISON") {
+    if (taskData?.GLOBAL_AVERAGE && taskData?.TIME_INVESTED_LAST_TASK) {
+      // BASIC_POINTS + (TIME_INVESTED_LAST_TASK > GLOBAL_AVERAGE ? SMALLER_BONUS : BONUS_FACTOR)
+      return (
+        params.BASIC_POINTS +
+        (taskData.TIME_INVESTED_LAST_TASK > taskData.GLOBAL_AVERAGE
+          ? params.SMALLER_BONUS
+          : params.BONUS_FACTOR)
+      );
+    }
+    return -1;
+  }
+  if (formula === "FORMULA_USER_AVERAGE_COMPARISON") {
+    if (taskData?.USER_AVERAGE && taskData?.TIME_INVESTED_LAST_TASK) {
+      // BASIC_POINTS + (TIME_INVESTED_LAST_TASK > USER_AVERAGE ? SMALLER_BONUS : BONUS_FACTOR)
+      return (
+        params.BASIC_POINTS +
+        (taskData.TIME_INVESTED_LAST_TASK > taskData.USER_AVERAGE
+          ? params.SMALLER_BONUS
+          : params.BONUS_FACTOR)
+      );
+    }
+    return -1;
+  }
+  if (formula === "FORMULA_GLOBAL_AND_INDIVIDUAL_IMPROVEMENT") {
+    // BASIC_POINTS + WEIGHT_GLOBAL_IMPROVE * max[0, (GLOBAL_AVERAGE - TIME_INVESTED_LAST_TASK)/GLOBAL_AVERAGE] + WEIGHT_INDIVIDUAL_IMPROVE * max[0, (USER_AVERAGE - TIME_INVESTED_LAST_TASK)/USER_AVERAGE]
+    if (
+      taskData?.GLOBAL_AVERAGE &&
+      taskData?.TIME_INVESTED_LAST_TASK &&
+      taskData?.USER_AVERAGE
+    ) {
+      const factorIndividual =
+        taskData.USER_AVERAGE > 0
+          ? (taskData.USER_AVERAGE - taskData.TIME_INVESTED_LAST_TASK) /
+            taskData.USER_AVERAGE
+          : 0;
+      const factorGlobal =
+        taskData.GLOBAL_AVERAGE > 0
+          ? (taskData.GLOBAL_AVERAGE - taskData.TIME_INVESTED_LAST_TASK) /
+            taskData.GLOBAL_AVERAGE
+          : 0;
+
+      const positiveFactorIndividual = Math.max(0, factorIndividual);
+      const positiveFactorGlobal = Math.max(0, factorGlobal);
+
+      const points =
+        params.WEIGHT_INDIVIDUAL_IMPROVE * positiveFactorIndividual +
+        params.WEIGHT_GLOBAL_IMPROVE * positiveFactorGlobal;
+
+      return points;
+    }
+    return -1;
+  }
+  return -1;
 }
