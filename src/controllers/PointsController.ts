e9761282957ea38ff6d660c400d9939f29d42e67
fvergaracl <fvergaracl@gmail.com>
2023-11-23 11:16:58 +0100

    Refactor pointsRoutes to include Swagger documentation

--- src/controllers/PointsController.ts
+++ src/controllers/PointsController.ts
@@ -19,33 +19,216 @@ import {
 } from "../utils/parseAndComputeFormula";
 
 class PointsController {
-  /*
-  router.get("/:userId", PointsController.getUserPoints);
-router.get("/:userId/:taskId", PointsController.getUserPointsInTask);
-router.get("/:userId/:gameId", PointsController.getUserPointsInGame);
-  */
-
   static async getUserPoints(req: Request, res: Response) {
     try {
-      res.status(200).json({ message: "getUserPoints" });
+      const { userId } = req.params;
+      const { from, to } = req.query;
+      if (from && isNaN(new Date(String(from)).getTime())) {
+        return res.status(400).json({ message: "'from' is invalid" });
+      }
+      if (to && isNaN(new Date(String(to)).getTime())) {
+        return res.status(400).json({ message: "'to' is invalid" });
+      }
+
+      if (!userId) {
+        return res.status(400).json({ message: "idUser is required" });
+      }
+
+      const userData = await UserModel.findOne({ userId });
+      if (!userData) {
+        return res
+          .status(404)
+          .json({ message: "User doesn't exist", points: 0 });
+      }
+      const userActions = userData.actions;
+      if (!userActions) {
+        return res
+          .status(404)
+          .json({ message: "User doesn't have actions", points: 0 });
+      }
+
+      let query = { userId };
+      if (from) {
+        query = {
+          ...query,
+          createdAt: { $gte: new Date(String(from)) },
+        } as any;
+      }
+      if (to) {
+        query = { ...query, createdAt: { $lte: new Date(String(to)) } } as any;
+      }
+      if (from && to) {
+        query = {
+          ...query,
+          createdAt: {
+            $gte: new Date(String(from)),
+            $lte: new Date(String(to)),
+          },
+        } as any;
+      }
+
+      console.log({ query });
+
+      const usersPoints = await PointsModel.find(query);
+      if (!usersPoints) {
+        return res
+          .status(404)
+          .json({ message: "User doesn't have points", points: 0 });
+      }
+      const points = usersPoints.reduce((a, b) => a + (b?.points ?? 0), 0);
+      res.status(200).json({
+        message: "Sum of points",
+        points,
+      });
     } catch (error) {
-      res.status(500).json({ message: "getUserPoints -ERROR" });
+      res.status(500).json({ message: error, points: -1 });
     }
   }
 
   static async getUserPointsInTask(req: Request, res: Response) {
     try {
-      res.status(200).json({ message: "getUserPointsInTask" });
+      // /points/:userId/:idTask
+      // with from /points/:userId/:idTask?from=2021-05-01T00:00:00.000Z
+      // with to /points/:userId/:idTask?to=2021-05-01T00:00:00.000Z
+      // with from and to /points/:userId/:idTask?from=2021-05-01T00:00:00.000Z&to=2021-05-01T00:00:00.000Z
+      const { userId, idTask } = req.params;
+      const { from, to } = req.query;
+      if (from && isNaN(new Date(String(from)).getTime())) {
+        return res.status(400).json({ message: "'from' is invalid" });
+      }
+      if (to && isNaN(new Date(String(to)).getTime())) {
+        return res.status(400).json({ message: "'to' is invalid" });
+      }
+      if (!userId) {
+        return res.status(400).json({ message: "idUser is required" });
+      }
+      if (!idTask) {
+        return res.status(404).json({ message: "idTask not found" });
+      }
+
+      const userData = await UserModel.findOne({ userId });
+      if (!userData) {
+        return res
+          .status(404)
+          .json({ message: "User doesn't exist", points: 0 });
+      }
+      const userActions = userData.actions;
+      if (!userActions) {
+        return res
+          .status(404)
+          .json({ message: "User doesn't have actions", points: 0 });
+      }
+
+      let query = {
+        userId,
+        "task._id": idTask,
+      };
+
+      if (from) {
+        query = {
+          ...query,
+          createdAt: { $gte: new Date(String(from)) },
+        } as any;
+      }
+      if (to) {
+        query = { ...query, createdAt: { $lte: new Date(String(to)) } } as any;
+      }
+      if (from && to) {
+        query = {
+          ...query,
+          createdAt: {
+            $gte: new Date(String(from)),
+            $lte: new Date(String(to)),
+          },
+        } as any;
+      }
+
+      const usersTaskPoints = await PointsModel.find(query);
+      if (!usersTaskPoints) {
+        return res.status(404).json({
+          message: "User doesn't have points in this task",
+          points: 0,
+        });
+      }
+      const points = usersTaskPoints.reduce((a, b) => a + (b?.points ?? 0), 0);
+      res.status(200).json({
+        message: "Sum of points in task",
+        points,
+      });
     } catch (error) {
-      res.status(500).json({ message: "getUserPointsInTask -ERROR" });
+      res.status(500).json({ message: error, points: -1 });
     }
   }
 
   static async getUserPointsInGame(req: Request, res: Response) {
     try {
-      res.status(200).json({ message: "getUserPointsInGame" });
+      const { userId, idGame } = req.params;
+      const { from, to } = req.query;
+      if (from && isNaN(new Date(String(from)).getTime())) {
+        return res.status(400).json({ message: "'from' is invalid" });
+      }
+      if (to && isNaN(new Date(String(to)).getTime())) {
+        return res.status(400).json({ message: "'to' is invalid" });
+      }
+
+      if (!userId) {
+        return res.status(400).json({ message: "idUser is required" });
+      }
+      if (!idGame) {
+        return res.status(404).json({ message: "idGame not found" });
+      }
+
+      const userData = await UserModel.findOne({ userId });
+      if (!userData) {
+        return res
+          .status(404)
+          .json({ message: "User doesn't exist", points: 0 });
+      }
+      const userActions = userData.actions;
+      if (!userActions) {
+        return res
+          .status(404)
+          .json({ message: "User doesn't have actions", points: 0 });
+      }
+
+      let query = {
+        userId,
+        "game._id": idGame,
+      };
+
+      if (from) {
+        query = {
+          ...query,
+          createdAt: { $gte: new Date(String(from)) },
+        } as any;
+      }
+      if (to) {
+        query = { ...query, createdAt: { $lte: new Date(String(to)) } } as any;
+      }
+      if (from && to) {
+        query = {
+          ...query,
+          createdAt: {
+            $gte: new Date(String(from)),
+            $lte: new Date(String(to)),
+          },
+        } as any;
+      }
+
+      const usersGamePoints = await PointsModel.find(query);
+      if (!usersGamePoints) {
+        return res.status(404).json({
+          message: "User doesn't have points in this game",
+          points: 0,
+        });
+      }
+      const points = usersGamePoints.reduce((a, b) => a + (b?.points ?? 0), 0);
+      res.status(200).json({
+        message: "Sum of points in game",
+        points,
+      });
     } catch (error) {
-      res.status(500).json({ message: "getUserPointsInGame -ERROR" });
+      res.status(500).json({ message: error, points: -1 });
     }
   }
 
@@ -61,7 +244,6 @@ router.get("/:userId/:gameId", PointsController.getUserPointsInGame);
       if (!idGame) {
         return res.status(404).json({ message: "Game not found" });
       }
-
 
       let game = await GameModel.findById(idGame);
       if (!game) {
@@ -159,20 +341,32 @@ router.get("/:userId/:gameId", PointsController.getUserPointsInGame);
         a.points > b.points ? a : b
       );
 
+      const points = pointsAssigned?.points ?? 0;
+      const formula = pointsAssigned?.formula ?? "No formula";
+      if (points < 0) {
+        // negative points because the formula's strategy is not correct
+        return res.status(400).json({
+          message: "Error in calculation",
+          points: pointsAssigned?.points,
+          formula: pointsAssigned?.formula,
+          userIsCreated,
+        });
+      }
+
       const newPoints = new PointsModel({
         userId,
         game,
         task,
-        points: pointsAssigned?.points,
-        formula: pointsAssigned?.formula,
+        points,
+        formula,
       });
       const newPointsSaved = await newPoints.save();
 
       // update user with new action
       const newAction = {
         timestamp: new Date(),
-        points: pointsAssigned?.points,
-        formula: pointsAssigned?.formula,
+        points,
+        formula,
       };
       const updatedUser = await UserModel.findByIdAndUpdate(
         user?._id,
@@ -184,8 +378,8 @@ router.get("/:userId/:gameId", PointsController.getUserPointsInGame);
 
       res.status(201).json({
         message: "Points assigned successfully",
-        points: pointsAssigned?.points,
-        formula: pointsAssigned?.formula,
+        points,
+        formula,
         userIsCreated,
       });
     } catch (error) {
--- src/routes/pointsRoutes.ts
+++ src/routes/pointsRoutes.ts
@@ -2,14 +2,205 @@ import express from "express";
 import { PointsController } from "../controllers/PointsController";
 
 const router = express.Router();
-// GET points/:userId (all points for a user)
-// GET points/:userId/:taskId (points for a user in a task)
-// GET points/:userId/:gameId (all points for a user in a game)
-// POST points/:userId/:taskId (create points for a user in a task)
 
+/**
+ * @swagger
+ * /points/{userId}:
+ *   get:
+ *     tags: [Points]
+ *     summary: Retrieve the total points of a user within a specified time range
+ *     description: >
+ *       This endpoint retrieves the total points accumulated by a user across all games and tasks,
+ *       optionally within a specified time range. It requires the user ID, and optionally 'from' and 'to'
+ *       date parameters, checks if the user exists, and sums up all the points assigned to the user.
+ *     parameters:
+ *       - in: path
+ *         name: userId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: ID of the user whose total points are to be retrieved.
+ *       - in: query
+ *         name: from
+ *         required: false
+ *         schema:
+ *           type: string
+ *           format: date-time
+ *           example: 2023-11-23T00:00:00Z
+ *         description: Start date to filter the points calculation. Should be in ISO format.
+ *       - in: query
+ *         name: to
+ *         required: false
+ *         schema:
+ *           type: string
+ *           format: date-time
+ *           example: 2023-12-31T23:59:59Z
+ *         description: End date to filter the points calculation. Should be in ISO format.
+ *     responses:
+ *       200:
+ *         description: Total sum of points successfully retrieved
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                 points:
+ *                   type: number
+ *                   description: Total points accumulated by the user.
+ *       400:
+ *         description: User ID is required, or 'from'/'to' date parameters are invalid
+ *       404:
+ *         description: User not found, or user doesn't have actions/points
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                 points:
+ *                   type: number
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                 points:
+ *                   type: number
+ */
 router.get("/:userId", PointsController.getUserPoints);
-router.get("/:userId/:taskId", PointsController.getUserPointsInTask);
-router.get("/:userId/:gameId", PointsController.getUserPointsInGame);
+
+/**
+ * @swagger
+ * /points/{userId}/task/{idTask}:
+ *   get:
+ *     tags: [Points]
+ *     summary: Retrieve the total points of a user for a specific task
+ *     description: >
+ *       This endpoint retrieves the total points accumulated by a user for a specific task.
+ *       It requires both user ID and task ID, checks if the user exists, and sums up
+ *       the points assigned for that task.
+ *     parameters:
+ *       - in: path
+ *         name: userId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: ID of the user whose points for the task are to be retrieved.
+ *       - in: path
+ *         name: idTask
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: ID of the task for which points are being retrieved.
+ *     responses:
+ *       200:
+ *         description: Sum of points for the task successfully retrieved
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                 points:
+ *                   type: number
+ *                   description: Total points accumulated by the user for the task.
+ *       400:
+ *         description: User ID is required but was not provided
+ *       404:
+ *         description: User or task not found, or user doesn't have actions/points for this task
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                 points:
+ *                   type: number
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                 points:
+ *                   type: number
+ */
+router.get("/:userId/task/:idTask", PointsController.getUserPointsInTask);
+
+/**
+ * @swagger
+ * /points/{userId}/game/{idGame}:
+ *   get:
+ *     tags: [Points]
+ *     summary: Retrieve the total points of a user in a specific game
+ *     description: >
+ *       This endpoint retrieves the total points accumulated by a user in a specified game.
+ *       It requires both user ID and game ID, checks if the user exists, and calculates
+ *       the sum of points assigned in the game.
+ *     parameters:
+ *       - in: path
+ *         name: userId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: ID of the user whose points are to be retrieved.
+ *       - in: path
+ *         name: idGame
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: ID of the game for which points are being retrieved.
+ *     responses:
+ *       200:
+ *         description: Sum of points in the game successfully retrieved
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                 points:
+ *                   type: number
+ *                   description: Total points accumulated by the user in the game.
+ *       400:
+ *         description: User ID is required but was not provided
+ *       404:
+ *         description: User or game not found, or user doesn't have actions/points in this game
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                 points:
+ *                   type: number
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                 points:
+ *                   type: number
+ */
+router.get("/:userId/game/:idGame", PointsController.getUserPointsInGame);
 
 /**
  * @swagger
@@ -59,7 +250,7 @@ router.get("/:userId/:gameId", PointsController.getUserPointsInGame);
  *                   type: boolean
  *                   description: Indicates if a new user was created during the process.
  *       400:
- *         description: User ID is required but was not provided
+ *         description: User ID is required but was not provided or points isn't calculated(because formula's strategy is not valid)
  *       404:
  *         description: Game, Task, or Strategy not found
  *       500:
@@ -121,7 +312,7 @@ router.post("/:idGame", PointsController.assignPointsToUser);
  *                   type: boolean
  *                   description: Indicates if a new user was created during the process.
  *       400:
- *         description: User ID is required but was not provided
+ *         description: User ID is required but was not provided or points isn't calculated(because formula's strategy is not valid)
  *       404:
  *         description: Game, Task, or Strategy not found or Task is not part of the Game
  *       500:
