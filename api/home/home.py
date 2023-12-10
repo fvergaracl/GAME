4d151f1100b8a0c1d474690b7dc6566909550deb
fvergaracl <fvergaracl@gmail.com>
2023-12-10 22:55:18 +0100

    Update API response in home.py

--- api/home/home.py
+++ api/home/home.py
@@ -1,5 +1,5 @@
-from fastapi import APIRouter, Response, Depends
-
+from fastapi import APIRouter, Depends
+from fastapi.responses import JSONResponse
 from core.fastapi.dependencies import PermissionDependency, AllowAll
 
 home_router = APIRouter()
@@ -7,4 +7,8 @@ home_router = APIRouter()
 
 @home_router.get("/health", dependencies=[Depends(PermissionDependency([AllowAll]))])
 async def home():
-    return Response(status_code=200)
+    json_response = {"GAME": "Goals And Motivation Engine" , "version": "0.0.1"}
+    return JSONResponse(
+        status_code=200,
+        content=json_response
+    )
