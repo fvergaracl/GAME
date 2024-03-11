1846b8d14f5ea2a18e0b99d0967354f65b6cb5eb
fvergaracl <fvergaracl@gmail.com>
2024-03-11 22:40:29 +0100

    Refactor game_params.py and games_params_schema.py

--- app/model/game_params.py
+++ app/model/game_params.py
@@ -9,12 +9,12 @@ class GamesParams(BaseModel, table=True):
     Represents the parameters for a game.
 
     Attributes:
-        paramKey (str): The key of the parameter.
+        key (str): The key of the parameter.
         value (str): The value of the parameter.
         gameId (str): The ID of the game associated with the parameter.
     """
 
-    paramKey: str = Field(sa_column=Column(String))
+    key: str = Field(sa_column=Column(String))
     value: str = Field(sa_column=Column(String))
     gameId: str = Field(
         sa_column=Column(UUID(as_uuid=True), ForeignKey("games.id")))
@@ -23,22 +23,18 @@ class GamesParams(BaseModel, table=True):
         orm_mode = True
 
     def __str__(self):
-        return f"GameParams: (id={self.id}, "
-    "created_at={self.created_at}, updated_at={self.updated_at}, "
-    "paramKey={self.paramKey}, value={self.value}, gameId={self.gameId})"
+        return f"GameParams: (id={self.id}, created_at={self.created_at}, updated_at={self.updated_at}, key={self.key}, value={self.value}, gameId={self.gameId})"
 
     def __repr__(self):
-        return f"GameParams: (id={self.id}, created_at={self.created_at}, "
-    "updated_at={self.updated_at}, paramKey={self.paramKey}, "
-    "value={self.value}, gameId={self.gameId})"
+        return f"GameParams: (id={self.id}, created_at={self.created_at}, updated_at={self.updated_at}, key={self.key}, value={self.value}, gameId={self.gameId})" 
 
     def __eq__(self, other):
         return (
             isinstance(other, GamesParams)
-            and self.paramKey == other.paramKey
+            and self.key == other.key
             and self.value == other.value
             and self.gameId == other.gameId
         )
 
     def __hash__(self):
-        return hash((self.paramKey, self.value, self.gameId))
+        return hash((self.key, self.value, self.gameId))
