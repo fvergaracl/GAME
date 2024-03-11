a7356ca0a3e9c8f7fead4eafd8866d286d3bdca4
fvergaracl <fvergaracl@gmail.com>
2024-03-11 10:56:46 +0100

    Refactor strategy service and engine initialization

--- app/services/strategy_service.py
+++ app/services/strategy_service.py
@@ -1,15 +1,17 @@
 from app.core.exceptions import ConflictError
 from app.repository.strategy_repository import StrategyRepository
 from app.services.base_service import BaseService
-from app.engine.import_app_engine_modules import import_app_engine_modules
+from app.engine.all_engine_strategies import all_engine_strategies
+
 
 class StrategyService(BaseService):
     def __init__(self, strategy_repository: StrategyRepository):
         self.strategy_repository = strategy_repository
         super().__init__(strategy_repository)
 
-    def list_all_strategies(self, schema):
-        all_strategies = import_app_engine_modules
+    def list_all_strategies(self):
+        all_strategies = all_engine_strategies()
+        print(all_strategies)
         pass
 
     def get_strategy_by_id(self, id):
