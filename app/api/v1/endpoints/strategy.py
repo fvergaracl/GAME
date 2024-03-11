c5dd92d729b9313351d093d4b73303a57d40f90b
fvergaracl <fvergaracl@gmail.com>
2024-03-11 13:03:06 +0100

    Refactor strategy endpoint and add get_strategy_by_id

--- app/api/v1/endpoints/strategy.py
+++ app/api/v1/endpoints/strategy.py
@@ -5,10 +5,9 @@ from typing import List
 from app.core.container import Container
 from app.schema.base_schema import FindBase
 from app.schema.rules_schema import ResponseFindAllRuleVariables
-from app.schema.strategy_schema import (CreateStrategyPost,
-                                        CreateStrategyResult,
-                                        Strategy,
-                                        FindStrategyResult)
+from app.schema.strategy_schema import (
+    Strategy,
+    FindStrategyResult)
 from app.services.rules_service import RulesService
 from app.services.strategy_service import StrategyService
 
@@ -29,25 +28,13 @@ def get_strategy_list(
     return response
 
 
-# @router.get("{id}", response_model=FindStrategyResult)
-# @inject
-# def get_strategy_by_id(
-#     id: str,
-#     service: StrategyService = Depends(Provide[Container.strategy_service]),
-# ):
-#     return service.get_strategy_by_id(id)
-
-
-# @router.post("", response_model=CreateStrategyResult)
-# @inject
-# def create_strategy(
-#     schema: CreateStrategyPost,
-#     service: StrategyService = Depends(Provide[Container.strategy_service]),
-#     service_rules: RulesService = Depends(Provide[Container.rules_service]),
-# ):
-#     all_variables = service_rules.get_all_variables()
-#     all_variables = [variable.get_data() for variable in all_variables]
-#     return service.create_strategy(schema)
+@router.get("/{id}", response_model=Strategy)
+@inject
+def get_strategy_by_id(
+    id: str,
+    service: StrategyService = Depends(Provide[Container.strategy_service]),
+):
+    return service.get_strategy_by_id(id)
 
 
 # @router.get("rules/variable", response_model=ResponseFindAllRuleVariables)
