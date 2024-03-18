3e30b003e563308dfd7e15af62c78446e43e7be0
fvergaracl <fvergaracl@gmail.com>
2024-03-18 01:22:44 +0100

    Refactor user points schema and repository

--- app/api/v1/endpoints/users.py
+++ app/api/v1/endpoints/users.py
@@ -2,10 +2,10 @@ from uuid import UUID
 
 from dependency_injector.wiring import Provide, inject
 from fastapi import APIRouter, Depends, Query
-
+from typing import List
 from app.core.container import Container
 from app.schema.base_schema import FindBase
-from app.schema.user_points_schema import UserPointsAssigned
+from app.schema.user_points_schema import AllPointsByGame
 from app.schema.user_schema import (CreatedUser, PostAssignPointsToUser,
                                     PostCreateUser,
                                     PostPointsConversionRequest,
@@ -13,11 +13,53 @@ from app.schema.user_schema import (CreatedUser, PostAssignPointsToUser,
                                     ResponsePointsConversion, UserPointsTasks,
                                     UserWallet)
 from app.services.user_service import UserService
+from app.services.user_points_service import UserPointsService
+from app.services.task_service import TaskService
 
 router = APIRouter(
     prefix="/users",
     tags=["users"],
 )
+
+summary_get_user_points = "Get user points"
+description_get_user_points = """
+## Get user points
+### Get user points
+"""
+
+
+@router.get(
+    "/{externalUserId}/points",
+    response_model=List[AllPointsByGame],
+    summary=summary_get_user_points,
+    description=description_get_user_points,
+)
+@inject
+def get_points_by_user_id(
+    externalUserId: str,
+    service: UserPointsService = Depends(Provide[Container.user_points_service]),
+):
+    return service.get_points_by_externalUserId(externalUserId)
+
+
+summary_get_user_wallet = "Get user wallet"
+description_get_user_wallet = """
+## Get user wallet
+### Get user wallet
+"""
+
+@router.get(
+    "/{externalUserId}/wallet",
+    response_model=UserWallet,
+    summary=summary_get_user_wallet,
+    description=description_get_user_wallet,
+)
+@inject
+def get_wallet_by_user_id(
+    externalUserId: str,
+    service: UserService = Depends(Provide[Container.user_service]),
+):
+    return service.get_wallet_by_externalUserId(externalUserId)
 
 
 # @router.get("")
@@ -60,11 +102,6 @@ router = APIRouter(
 #     return service.assign_points_to_user(userId, schema)
 
 
-# summary_get_wallet_by_user_id = "Get wallet by user id"
-# description_get_wallet_by_user_id = """
-# ## Get wallet by user id
-# ### Get wallet by user id
-# """
 
 # summary_get_points = "Get points by user id"
 # description_get_points = """
@@ -87,60 +124,48 @@ router = APIRouter(
 #     return service.get_points_by_user_id(userId)
 
 
-# @router.get(
-#     "/{userId}/wallet",
-#     response_model=UserWallet,
-#     summary=summary_get_wallet_by_user_id,
-#     description=description_get_wallet_by_user_id,
-# )
-# @inject
-# def get_wallet_by_user_id(
-#     userId: UUID,
-#     service: UserService = Depends(Provide[Container.user_service]),
-# ):
-#     return service.get_wallet_by_user_id(userId)
 
 
-# summary_preview_points = "Preview Points to Coins Conversion"
-# description_preview_points = """## Preview Points to Coins Conversion
-# ### Provides a preview of the points to coins conversion for a specific user.
-# """
+summary_preview_points = "Preview Points to Coins Conversion"
+description_preview_points = """## Preview Points to Coins Conversion
+### Provides a preview of the points to coins conversion for a specific user.
+"""
 
 
-# @router.get(
-#     "/{userId}/convert/preview",
-#     response_model=ResponseConversionPreview,
-#     summary=summary_preview_points,
-#     description=description_preview_points,
-# )
-# @inject
-# def preview_points_to_coins_conversion(
-#     userId: UUID,
-#     points: int = Query(...,
-#                         description="The number of points to convert to coins"),
-#     service: UserService = Depends(Provide[Container.user_service]),
-# ):
-#     return service.preview_points_to_coins_conversion(userId, points)
+@router.get(
+    "/{externalUserId}/convert/preview",
+    response_model=ResponseConversionPreview,
+    summary=summary_preview_points,
+    description=description_preview_points,
+)
+@inject
+def preview_points_to_coins_conversion(
+    externalUserId: str,
+    points: int = Query(...,
+                        description="The number of points to convert to coins"),
+    service: UserService = Depends(Provide[Container.user_service]),
+):
+    return service.preview_points_to_coins_conversion_externalUserId(
+        externalUserId, points
+    )
 
 
-# summary_convert_points = "Convert Points to Coins"
-# description_convert_points = """## Convert Points to Coins
-# ### Performs the actual conversion of points to coins for the specified user.
-# """
+summary_convert_points = "Convert Points to Coins"
+description_convert_points = """## Convert Points to Coins
+### Performs the actual conversion of points to coins for the specified user.
+"""
 
 
-# @router.post(
-#     "/{userId}/convert",
-#     response_model=ResponsePointsConversion,
-#     summary=summary_convert_points,
-#     description=description_convert_points,
-# )
-# @inject
-# def convert_points_to_coins(
-#     userId: UUID,
-#     schema: PostPointsConversionRequest,
-#     service: UserService = Depends(Provide[Container.user_service]),
-# ):
-#     # Logic to perform conversion should be implemented in UserService or a
-#     # dedicated service.
-#     return service.convert_points_to_coins(userId, schema)
+@router.post(
+    "/{externalUserId}/convert",
+    response_model=ResponsePointsConversion,
+    summary=summary_convert_points,
+    description=description_convert_points,
+)
+@inject
+def convert_points_to_coins(
+    externalUserId: str,
+    schema: PostPointsConversionRequest,
+    service: UserService = Depends(Provide[Container.user_service]),
+):
+    return service.convert_points_to_coins_externalUserId(externalUserId, schema)
