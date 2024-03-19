a92cf19b7775cb299d3a63f994e8485d96af1f07
fvergaracl <fvergaracl@gmail.com>
2024-03-19 17:34:27 +0100

    Add eralchemy package and its dependencies

--- app/engine/default.py
+++ app/engine/default.py
@@ -1,72 +1,7 @@
 """
 # noqa
-### Enhanced Gamification Scenarios, Points Allocation Table with Tags, Required Variables, and Condition Checks 
+https://dreampuf.github.io/GraphvizOnline/#digraph%20G%20%7B%0A%20%20%20%20rankdir%3DTB%3B%0A%20%20%20%20%0A%20%20%20%20node%20%5Bshape%3Dbox%2C%20style%3Dfilled%2C%20fillcolor%3Dlightgray%5D%3B%0A%20%20%20%20edge%20%5Bfontsize%3D10%5D%3B%0A%0A%20%20%20%20start%20%5Blabel%3D%22Start%22%2C%20shape%3Dellipse%2C%20fillcolor%3Dlightgray%5D%3B%0A%20%20%20%20is1or2%20%5Blabel%3D%221st%20or%202nd%20Measure%3F%22%5D%3B%0A%20%20%20%20is2nd%20%5Blabel%3D%222nd%20Measure%3F%22%5D%3B%0A%20%20%20%20subseq%20%5Blabel%3D%22Subsequent%3F%22%5D%3B%0A%20%20%20%20evalTime%20%5Blabel%3D%22Eval%20Time%22%5D%3B%0A%20%20%20%20%0A%20%20%20%20case1%20%5Blabel%3D%22Case%201%22%2C%20shape%3Dparallelogram%2C%20fillcolor%3Dlightyellow%5D%3B%0A%20%20%20%20case2_1%20%5Blabel%3D%22Case%202.1%22%2C%20shape%3Dparallelogram%2C%20fillcolor%3Dlightyellow%5D%3B%0A%20%20%20%20case2_2%20%5Blabel%3D%22Case%202.2%22%2C%20shape%3Dparallelogram%2C%20fillcolor%3Dlightyellow%5D%3B%0A%20%20%20%20case3%20%5Blabel%3D%22Case%203%22%2C%20shape%3Dparallelogram%2C%20fillcolor%3Dlightyellow%5D%3B%0A%20%20%20%20case4_1%20%5Blabel%3D%22Case%204.1%22%2C%20shape%3Dparallelogram%2C%20fillcolor%3Dlightyellow%5D%3B%0A%20%20%20%20case4_2%20%5Blabel%3D%22Case%204.2%22%2C%20shape%3Dparallelogram%2C%20fillcolor%3Dlightyellow%5D%3B%0A%20%20%20%20case4_3%20%5Blabel%3D%22Case%204.3%22%2C%20shape%3Dparallelogram%2C%20fillcolor%3Dlightyellow%5D%3B%0A%20%20%20%20case4_4%20%5Blabel%3D%22Case%204.4%22%2C%20shape%3Dparallelogram%2C%20fillcolor%3Dlightyellow%5D%3B%0A%20%20%20%20default%20%5Blabel%3D%22Default%22%2C%20shape%3Dparallelogram%2C%20fillcolor%3Dlightyellow%5D%3B%0A%0A%20%20%20%20start%20-%3E%20is1or2%0A%20%20%20%20is1or2%20-%3E%20case1%20%5Blabel%3D%22Yes%22%5D%0A%20%20%20%20is1or2%20-%3E%20is2nd%20%5Blabel%3D%22No%22%5D%0A%20%20%20%20is2nd%20-%3E%20case2_1%20%5Blabel%3D%22Yes%2C%20%3C%20Global%22%5D%0A%20%20%20%20is2nd%20-%3E%20case2_2%20%5Blabel%3D%22Yes%2C%20%3E%3D%20Global%22%5D%0A%20%20%20%20is2nd%20-%3E%20subseq%20%5Blabel%3D%22No%22%5D%0A%20%20%20%20subseq%20-%3E%20evalTime%0A%20%20%20%20evalTime%20-%3E%20case3%20%5Blabel%3D%22%5B%3C%20Indiv%5D%22%5D%0A%20%20%20%20evalTime%20-%3E%20case4_1%20%5Blabel%3D%22%5B%3C%20Indiv%2C%20%3E%20Global%5D%22%5D%0A%20%20%20%20evalTime%20-%3E%20case4_2%20%5Blabel%3D%22%5B%3E%20Both%20Avgs%5D%22%5D%0A%20%20%20%20evalTime%20-%3E%20case4_3%20%5Blabel%3D%22%5B%3C%20Both%20Avgs%5D%22%5D%0A%20%20%20%20evalTime%20-%3E%20case4_4%20%5Blabel%3D%22%5B%3E%20Indiv%2C%20%3C%3D%20Global%5D%22%5D%0A%20%20%20%20evalTime%20-%3E%20default%20%5Blabel%3D%22Else%22%5D%0A%7D%0A
 
-#### Basic Engagement Scenario
-
-| Case/Subcase | Conditions                                                              | Points Awarded                                        | Tag               | Required Variables            | Condition Check |
-| ------------ | ----------------------------------------------------------------------- | ----------------------------------------------------- | ----------------- | ----------------------------- | --------------- |
-| Case 1       | First/second measurement without any prior users having 2 measurements. | variable_basic_points | `BasicEngagement` | `default_points_task_campaign` | None Required   |
-
-#### Comparative Performance Scenarios
-
-| Case/Subcase | Conditions                                               | Points Awarded          | Tag                | Required Variables                                                            | Condition Check                               |
-| ------------ | -------------------------------------------------------- | ----------------------- | ------------------ | ----------------------------------------------------------------------------- | --------------------------------------------- |
-| Case 2       | Second measurement with time taken < global calculation. | variable_basic_points + variable_bonus_points.    | `PerformanceBonus` | `default_points_task_campaign`, `tiempo_tardado_ultima_task`, `calculo_global` | `tiempo_tardado_ultima_task < calculo_global` |
-
-#### Individual Improvement Scenario
-
-| Case/Subcase | Conditions                                                  | Points Awarded        | Tag                    | Required Variables                                                                | Condition Check                                    |
-| ------------ | ----------------------------------------------------------- | --------------------- | ---------------------- | --------------------------------------------------------------------------------- | -------------------------------------------------- |
-| Case 3       | Comparison with individual calculation (greater or lesser). | variable_individual_adjustment_points         | `IndividualAdjustment` | `default_points_task_campaign`, `tiempo_tardado_ultima_task`, `calculo_individual` | `tiempo_tardado_ultima_task <> calculo_individual` |
-
-#### Advanced Gamification Strategies
-
-| Case/Subcase | Conditions                                           | Points Awarded                                                                    | Tag                         | Required Variables                                                                                  | Condition Check                                                                                  |
-| ------------ | ---------------------------------------------------- | --------------------------------------------------------------------------------- | --------------------------- | --------------------------------------------------------------------------------------------------- | ------------------------------------------------------------------------------------------------ |
-| Case 4.1     | Individual improvement but below the global average. | variable_individual_over_global_points | `IndividualOverGlobal`      | `default_points_task_campaign`, `tiempo_tardado_ultima_task`, `calculo_individual`, `calculo_global` | `tiempo_tardado_ultima_task < calculo_individual && tiempo_tardado_ultima_task > calculo_global` |
-| Case 4.2     | Individual improvement and above the global average. | variable_peak_performer_bonus_points                                               | `PeakPerformerBonus`        | `default_points_task_campaign`, `tiempo_tardado_ultima_task`, `calculo_individual`, `calculo_global` | `tiempo_tardado_ultima_task < calculo_individual && tiempo_tardado_ultima_task < calculo_global` |
-| Case 4.3     | Individual worsening, but above the global average.  | variable_global_advantage_adjustment_points                                  | `GlobalAdvantageAdjustment` | `default_points_task_campaign`, `tiempo_tardado_ultima_task`, `calculo_individual`, `calculo_global` | `tiempo_tardado_ultima_task > calculo_individual && tiempo_tardado_ultima_task < calculo_global` |
-#### Consistent Performance Bonus
-
-| Case/Subcase | Conditions                                                             | Points Awarded                  | Tag                  | Required Variables                                                                                        | Condition Check                                                                 |
-| ------------ | ---------------------------------------------------------------------- | ------------------------------- | -------------------- | --------------------------------------------------------------------------------------------------------- | ------------------------------------------------------------------------------- |
-| Case 5       | User maintains or improves their performance for 3 consecutive tasks. | variable_consistent_performance_bonus_points | `ConsistentPerformance` | `default_points_task_campaign`, `tiempo_tardado_ultimas_tres_tasks`, `promedio_tiempo_tardado_tres_tasks_anteriores`, `promedio_tiempo_tardado_todos_usuarios_tres_tasks_anteriores` | `tiempo_tardado_ultimas_tres_tasks <= promedio_tiempo_tardado_tres_tasks_anteriores && tiempo_tardado_ultimas_tres_tasks < promedio_tiempo_tardado_todos_usuarios_tres_tasks_anteriores` |
-
-Gamification System Decision Tree
-.
-├── Is it the user's first or second measurement in the task in general?
-│ ├── Yes
-│ │ └── Use Case 1: `BasicEngagement` | points = variable_basic_points
-│ └── No
-│ └── Is this the user's second measurement?
-│ ├── Yes
-│ │ └── Is the time taken for the last task less than the global calculation?
-│ │ └── Yes
-│ │ └── Use Case 2: `PerformanceBonus` | points = variable_basic_points + variable_bonus_points
-│ └── No (It's a subsequent measurement)
-│ └── Is the time taken for the last task greater or less than the individual calculation?
-│ ├── Greater or equal
-│ │ └── Evaluate against both, individual and global calculations
-│ │ ├── If time is less than the individual calculation AND greater than the global calculation
-│ │ │ └── Use Case 4.1: `IndividualOverGlobal` | points = variable_individual_over_global_points
-│ │ ├── If time is less than both, individual and global calculations
-│ │ │ └── Use Case 4.2: `PeakPerformerBonus` | points = variable_peak_performer_bonus_points
-│ │ └── If time is greater than the individual calculation but less than the global calculation
-│ │ └── Use Case 4.3: `GlobalAdvantageAdjustment` | points = variable_global_advantage_adjustment_points
-│ └── Less
-│ └── Use Case 3: `IndividualAdjustment` | points = variable_individual_adjustment_points
-
-# EXAMPLE
-
-| Function Name                        | Description                                                                                     | Points Awarded |
-| ------------------------------------ | ----------------------------------------------------------------------------------------------- | -------------- |
-| `basic_engagement_points`            | Fixed number of points for a user's initial actions within the gamification system.             | 1              |
-| `performance_bonus_points`           | Additional points awarded for performance above a certain threshold.                            | 10             |
-| `individual_over_global_points`      | Additional points for users who have improved their performance compared to their own history.  | 3              |
-| `peak_performer_bonus_points`        | Bonus points for users exceeding both their individual performance and the global average.      | 15             |
-| `global_advantage_adjustment_points` | Additional points for users above the global average but with decreased individual performance. | 7              |
-| `individual_adjustment_points`       | Points awarded for users who have improved their individual performance.                        | 8              |
 """
 
 from app.engine.base_strategy import BaseStrategy
