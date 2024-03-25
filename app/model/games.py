34902733ec4514fa8aafa3b636e55da68ac1bd74
fvergaracl <fvergaracl@gmail.com>
2024-03-25 11:29:22 +0100

    Remove endDateTime field from Games model

--- app/model/games.py
+++ app/model/games.py
@@ -12,33 +12,28 @@ class Games(BaseModel, table=True):
     Attributes:
         externalGameId (str): The external ID of the game.
         platform (str): The platform on which the game is played.
-        endDateTime (datetime): The end date and time of the game.
     """
 
     externalGameId: str = Field(sa_column=Column(String, unique=True))
     strategyId: str = Field(sa_column=Column(
         String), nullable=False, default="default")
     platform: str = Field(sa_column=Column(String), nullable=False)
-    endDateTime: datetime = Field(
-        sa_column=Column(DateTime(timezone=True), nullable=True)
-    )
 
     class Config:  # noqa
         orm_mode = True  # noqa
 
     def __str__(self):
-        return f" Games: (id= {self.id}, created_at= {self.created_at}, updated_at= {self.updated_at}, externalGameId= {self.externalGameId}, strategyId= {self.strategyId}, platform= {self.platform}, endDateTime= {self.endDateTime})"
+        return f" Games: (id= {self.id}, created_at= {self.created_at}, updated_at= {self.updated_at}, externalGameId= {self.externalGameId}, strategyId= {self.strategyId}, platform= {self.platform})"  # noqa
 
     def __repr__(self):
-        return f" Games: (id= {self.id}, created_at= {self.created_at}, updated_at= {self.updated_at}, externalGameId= {self.externalGameId}, strategyId= {self.strategyId}, platform= {self.platform}, endDateTime= {self.endDateTime})"
+        return f" Games: (id= {self.id}, created_at= {self.created_at}, updated_at= {self.updated_at}, externalGameId= {self.externalGameId}, strategyId= {self.strategyId}, platform= {self.platform})"  # noqa
 
     def __eq__(self, other):
         return (
             isinstance(other, Games)
             and self.externalGameId == other.externalGameId
             and self.platform == other.platform
-            and self.endDateTime == other.endDateTime
         )
 
     def __hash__(self):
-        return hash((self.externalGameId, self.platform, self.endDateTime))
+        return hash((self.externalGameId, self.platform))
