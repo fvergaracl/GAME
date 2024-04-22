0333fc083d58cc6d0811386e306d9e703e1e39f8
fvergaracl <fvergaracl@gmail.com>
2024-04-22 13:51:59 +0200

    Refactor game_params.py model to include additional attributes and improve string representation

--- app/model/game_params.py
+++ app/model/game_params.py
@@ -9,24 +9,48 @@ class GamesParams(BaseModel, table=True):
     Represents the parameters for a game.
 
     Attributes:
-        key (str): The key of the parameter.
+        key (str): The key of the parameter, used as an identifier.
         value (str): The value of the parameter.
-        gameId (str): The ID of the game associated with the parameter.
+        gameId (str): The ID of the game associated with this parameter,
+          acting as a foreign key to the `games` table.
+
+    Methods:
+        __str__(self): Returns a string representation of the model instance,
+          including the key, value, and game ID.
+        __repr__(self): Returns an official string representation of the model.
+        __eq__(self, other): Determines equality based on key, value, and
+          gameId.
+        __hash__(self): Provides a hash based on the key, value, and gameId
+          attributes.
+
+    Configurations:
+        orm_mode (bool): Allows the model to be used with ORM, set to True to
+          enable.
     """
 
+    # Field definitions
     key: str = Field(sa_column=Column(String))
     value: str = Field(sa_column=Column(String))
     gameId: str = Field(
-        sa_column=Column(UUID(as_uuid=True), ForeignKey("games.id")))
+        sa_column=Column(UUID(as_uuid=True), ForeignKey("games.id"))
+    )
 
-    class Config:   # noqa
-        orm_mode = True  # noqa
+    class Config:  # SQLAlchemy configuration
+        orm_mode = True  # Enables ORM mode for SQLModel
 
     def __str__(self):
-        return f"GameParams: (id={self.id}, created_at={self.created_at}, updated_at={self.updated_at}, key={self.key}, value={self.value}, gameId={self.gameId})"
+        return (
+            f"GameParams: (id={self.id}, created_at={self.created_at}, "
+            f"updated_at={self.updated_at}, key={self.key}, "
+            f"value={self.value}, gameId={self.gameId})"
+        )
 
     def __repr__(self):
-        return f"GameParams: (id={self.id}, created_at={self.created_at}, updated_at={self.updated_at}, key={self.key}, value={self.value}, gameId={self.gameId})"
+        return (
+            f"GameParams: (id={self.id}, created_at={self.created_at}, "
+            f"updated_at={self.updated_at}, key={self.key}, "
+            f"value={self.value}, gameId={self.gameId})"
+        )
 
     def __eq__(self, other):
         return (
