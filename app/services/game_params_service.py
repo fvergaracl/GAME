98def7cdab4373bbfcdf29213eb001da171d180e
fvergaracl <fvergaracl@gmail.com>
2024-06-14 13:28:10 +0200

    chore: removed unused dependencies | docstrings added into the services

--- app/services/game_params_service.py
+++ app/services/game_params_service.py
@@ -3,6 +3,21 @@ from app.services.base_service import BaseService
 
 
 class GameParamsService(BaseService):
+    """
+    Service class for game parameters.
+
+    Attributes:
+        game_params_repository (GameParamsRepository): Repository instance for
+         game parameters.
+    """
+
     def __init__(self, game_params_repository: GameParamsRepository):
+        """
+        Initializes the GameParamsService with the provided repository.
+
+        Args:
+            game_params_repository (GameParamsRepository): The repository
+              instance.
+        """
         self.game_params_repository = game_params_repository
         super().__init__(game_params_repository)
--- app/services/strategy_service.py
+++ app/services/strategy_service.py
@@ -6,10 +6,26 @@ import os
 
 
 class StrategyService(BaseService):
+    """
+    Service class for managing strategies.
+
+    Attributes:
+        None
+    """
+
     def __init__(self):
+        """
+        Initializes the StrategyService.
+        """
         super().__init__(None)
 
     def list_all_strategies(self):
+        """
+        Lists all available strategies.
+
+        Returns:
+            list: A list of all strategies.
+        """
         all_unclean_strategies = all_engine_strategies()
         response = []
         for strategy in all_unclean_strategies:
@@ -26,6 +42,18 @@ class StrategyService(BaseService):
         return response
 
     def get_strategy_by_id(self, id):
+        """
+        Retrieves a strategy by its ID.
+
+        Args:
+            id (str): The ID of the strategy.
+
+        Returns:
+            dict: The strategy details.
+
+        Raises:
+            NotFoundError: If the strategy is not found.
+        """
         all_strategies = self.list_all_strategies()
         for strategy in all_strategies:
             if strategy["id"] == id:
@@ -35,6 +63,18 @@ class StrategyService(BaseService):
         )
 
     def get_Class_by_id(self, id):
+        """
+        Retrieves the class of a strategy by its ID.
+
+        Args:
+            id (str): The ID of the strategy.
+
+        Returns:
+            class: The strategy class.
+
+        Raises:
+            NotFoundError: If the strategy class is not found.
+        """
         all_strategies = all_engine_strategies()
         for strategy in all_strategies:
             file_class = inspect.getfile(strategy.__class__)
