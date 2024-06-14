b5d9554585f8354c30ead42f5365b65c7b8b3c4a
fvergaracl <fvergaracl@gmail.com>
2024-06-14 12:33:24 +0200

    chore: wallet schema modified

--- app/schema/wallet_schema.py
+++ app/schema/wallet_schema.py
@@ -6,16 +6,39 @@ from app.schema.base_schema import ModelBaseInfo
 
 
 class WalletWithoutUserId(BaseModel):
+    """
+    Model for wallet without user ID
+
+    Attributes:
+        coinsBalance (Optional[float]): Coins balance
+        pointsBalance (Optional[float]): Points balance
+        conversionRate (Optional[float]): Conversion rate
+    """
     coinsBalance: Optional[float]
     pointsBalance: Optional[float]
     conversionRate: Optional[float]
 
 
 class Wallet(WalletWithoutUserId):
+    """
+    Model for wallet
+
+    Attributes:
+        userId (Optional[str]): User ID
+    """
     userId: Optional[str]
 
 
 class BaseWallet(ModelBaseInfo):
+    """
+    Base model for wallet
+
+    Attributes:
+        coinsBalance (Optional[float]): Coins balance
+        pointsBalance (Optional[float]): Points balance
+        conversionRate (Optional[float]): Conversion rate
+        userId (Optional[int]): User ID
+    """
     coinsBalance: Optional[float]
     pointsBalance: Optional[float]
     conversionRate: Optional[float]
@@ -23,16 +46,41 @@ class BaseWallet(ModelBaseInfo):
 
 
 class BaseWalletOnlyUserId(BaseModel):
+    """
+    Model for wallet with only user ID
+
+    Attributes:
+        userId (int): User ID
+        pointsBalance (Optional[float]): Points balance
+    """
     userId: int
     pointsBalance: Optional[float]
 
 
 class PostPreviewConvertPoints(BaseModel):
+    """
+    Model for previewing points conversion
+
+    Attributes:
+        points (float): Points
+        externalUserId (str): External user ID
+    """
     points: float
     externalUserId: str
 
 
 class ResponsePreviewConvertPoints(BaseModel):
+    """
+    Model for points conversion preview response
+
+    Attributes:
+        coins (float): Coins
+        points_converted (float): Points converted
+        conversionRate (float): Conversion rate
+        afterConversionPoints (float): Points after conversion
+        afterConversionCoins (float): Coins after conversion
+        externalUserId (str): External user ID
+    """
     coins: float
     points_converted: float
     conversionRate: float
@@ -42,4 +90,5 @@ class ResponsePreviewConvertPoints(BaseModel):
 
 
 class CreateWallet(Wallet):
+    """Model for creating a wallet."""
     ...
