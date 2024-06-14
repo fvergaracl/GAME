104f5918b84391ca9fdfc9e0424a714c75ce8718
fvergaracl <fvergaracl@gmail.com>
2024-06-14 12:14:58 +0200

    feat: Modify of schema of base and game_params

--- app/schema/games_params_schema.py
+++ app/schema/games_params_schema.py
@@ -4,31 +4,59 @@ from pydantic import BaseModel
 
 
 class BaseGameParams(BaseModel):
+    """
+    Base model for game parameters
+
+    Attributes:
+        key (str): Parameter key
+        value (str | int | float | bool): Parameter value
+    """
     key: str
     value: str | int | float | bool
 
-    class Config:  # noqa
-        orm_mode = True  # noqa
+    class Config:
+        orm_mode = True
 
 
 class BaseCreateGameParams(BaseGameParams):
+    """Model for creating game parameters."""
     ...
 
 
 class InsertGameParams(BaseGameParams):
+    """
+    Model for inserting game parameters
+
+    Attributes:
+        gameId (str): Game ID
+    """
     gameId: str
 
 
 class CreateGameParams(BaseCreateGameParams):
+    """Model for creating game parameters."""
     ...
 
 
 class BaseFindGameParams(BaseGameParams):
-    ...
+    """
+    Model for finding game parameters
+
+    Attributes:
+        id (UUID): Unique identifier
+    """
     id: UUID
 
 
 class UpdateGameParams(CreateGameParams):
+    """
+    Model for updating game parameters
+
+    Attributes:
+        id (UUID): Unique identifier
+        key (str): Parameter key
+        value (str | int | float | bool): Parameter value
+    """
     id: UUID
     key: str
     value: str | int | float | bool
