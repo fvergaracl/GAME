2a6a5c73efe18090a15aee1e3af8ecda0daf3054
fvergaracl <fvergaracl@gmail.com>
2024-09-11 17:19:06 +0200

    logo added in dashboard

--- dashboard/src/components/AppSidebar.js
+++ dashboard/src/components/AppSidebar.js
@@ -14,7 +14,6 @@ import CIcon from '@coreui/icons-react'
 import { AppSidebarNav } from './AppSidebarNav'
 import keycloak from '../keycloak'
 import { logo } from 'src/assets/brand/logo'
-import { sygnet } from 'src/assets/brand/sygnet'
 
 // sidebar nav config
 import { _nav, _nav_administrator } from '../_nav'
@@ -53,7 +52,7 @@ const AppSidebar = () => {
       <CSidebarHeader className="border-bottom">
         <CSidebarBrand to="/">
           <CIcon customClassName="sidebar-brand-full" icon={logo} height={32} />
-          <CIcon customClassName="sidebar-brand-narrow" icon={sygnet} height={32} />
+          <CIcon customClassName="sidebar-brand-narrow" icon={logo} height={32} />
         </CSidebarBrand>
         <CCloseButton
           className="d-lg-none"
