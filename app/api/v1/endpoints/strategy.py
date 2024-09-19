49b97d388560c03fefb0b942b42fe413074a22e2
fvergaracl <fvergaracl@gmail.com>
2024-09-19 10:33:19 +0200

    feat  generate_logic_graph added , and generate graph by png

--- app/api/v1/endpoints/strategy.py
+++ app/api/v1/endpoints/strategy.py
@@ -2,11 +2,12 @@ from typing import List
 
 from dependency_injector.wiring import Provide, inject
 from fastapi import APIRouter, Depends
-
+from fastapi.responses import StreamingResponse
 from app.core.container import Container
 from app.core.exceptions import NotFoundError
 from app.schema.strategy_schema import Strategy
 from app.services.strategy_service import StrategyService
+import io
 
 router = APIRouter(
     prefix="/strategies",
@@ -77,3 +78,43 @@ def get_strategy_by_id(
         if strategy["id"] == id:
             return strategy
     raise NotFoundError(detail=f"Strategy not found with id: {id}")
+
+
+summary_get_strategy_graph_by_id = "Retrieve Strategy Graph by ID"
+description_get_strategy_graph_by_id = """
+## Retrieve Strategy Graph by ID
+### This endpoint retrieves the logic graph of a strategy using its unique ID.
+<sub>**Id_endpoint:** get_strategy_graph_by_id</sub>"""
+
+
+@router.get(
+    "/{id}/graph",
+    summary=summary_get_strategy_graph_by_id,
+    description=description_get_strategy_graph_by_id,
+)
+@inject
+def get_strategy_graph_by_id(
+    id: str,
+    service: StrategyService = Depends(Provide[Container.strategy_service]),
+):
+    """
+    Retrieve a strategy graph by its ID.
+
+    Args:
+        id (str): The ID of the strategy.
+        service (StrategyService): Injected StrategyService dependency.
+
+    Returns:
+        StreamingResponse: The logic graph of the specified strategy.
+    """
+    strategy = service.get_strategy_by_id(id)
+
+    if not strategy:
+        raise NotFoundError(detail=f"Strategy not found with id: {id}")
+    print(strategy)
+    strategy_class = service.get_Class_by_id(id)
+    dot = strategy_class.generate_logic_graph(format="png")
+
+    graph_png = dot.pipe(format="png")
+
+    return StreamingResponse(io.BytesIO(graph_png), media_type="image/png")
