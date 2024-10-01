9c4c972b14568fbb1dcf9a30003f3127cd7fe1e6
fvergaracl <fvergaracl@gmail.com>
2024-10-01 10:07:55 +0200

    Init KPI routes

--- app/api/v1/routes.py
+++ app/api/v1/routes.py
@@ -5,12 +5,12 @@ from starlette.routing import BaseRoute
 
 from app.api.v1.endpoints.apikey import router as apikey_router
 from app.api.v1.endpoints.games import router as game_router
+from app.api.v1.endpoints.kpi import router as kpi_router
 from app.api.v1.endpoints.strategy import router as strategy_router
 from app.api.v1.endpoints.tasks import router as task_router
 from app.api.v1.endpoints.userPoints import router as user_points_router
 from app.api.v1.endpoints.users import router as user_router
 from app.api.v1.endpoints.wallet import router as wallet_router
-
 routers = APIRouter()
 router_list = [
     apikey_router,
@@ -20,6 +20,7 @@ router_list = [
     user_points_router,
     user_router,
     wallet_router,
+    kpi_router,
 ]
 
 
