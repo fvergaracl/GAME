e3302249aa178563af1d919a77155d2a9397f9c5
fvergaracl <fvergaracl@gmail.com>
2024-10-01 10:07:55 +0200

    Init KPI routes

---
+++ app/api/v1/endpoints/kpi.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter
+from datetime import datetime
+
+router = APIRouter(
+    prefix="/kpi",
+    tags=["kpi"],
+)
+
+
+summary_health_check = "Health Check"
+description_health_check = """
+## Health Check
+### This endpoint checks the health of the KPI service.
+<sub>**Id_endpoint:** health_check</sub>"""
+
+
+@router.get(
+    "",
+    summary=summary_health_check,
+    description=description_health_check,
+)
+async def health_check():
+    """
+    Check the health of the KPI service.
+
+    Returns:
+        dict: The health status of the KPI service.
+    """
+    return {"status": "KPI service is running", "timestamp": datetime.utcnow()}
