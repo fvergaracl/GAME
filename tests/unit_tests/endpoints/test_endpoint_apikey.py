411b2b786a8f9339682794b03f1bd4641bce2962
fvergaracl <fvergaracl@gmail.com>
2024-10-03 14:31:13 +0200

    chore: Update error message for expired token in test_endpoint_apikey.py

--- tests/unit_tests/endpoints/test_endpoint_apikey.py
+++ tests/unit_tests/endpoints/test_endpoint_apikey.py
@@ -221,4 +221,4 @@ async def test_get_all_api_keys_token_expired(mock_api_key_service):
         )
 
         assert response.status_code == 401
-        assert response.json()["detail"] == "Not authenticated"
+        assert response.json()["detail"] == "Invalid token"
